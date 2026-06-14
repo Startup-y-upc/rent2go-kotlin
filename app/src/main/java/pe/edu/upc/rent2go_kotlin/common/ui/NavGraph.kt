80c651a79f174eee4298777a8e46e47aabad1182
feg06 <u202213468@upc.edu.pe>
2026-06-14 12:28:08 -0500

    feat(iam): make start destination dynamic to support auto-login

--- app/src/main/java/pe/edu/upc/rent2go_kotlin/common/ui/NavGraph.kt
+++ app/src/main/java/pe/edu/upc/rent2go_kotlin/common/ui/NavGraph.kt
@@ -16,9 +16,11 @@ import pe.edu.upc.rent2go_kotlin.iam.presentation.AuthViewModel
 fun SetupNavGraph(navController: NavHostController) {
     val authViewModel: AuthViewModel = viewModel()
 
+    val startDestination = if (authViewModel.currentUser != null) "car_list" else "login"
+
     NavHost(
         navController = navController,
-        startDestination = "login"
+        startDestination = startDestination
     ) {
         composable(route = "login") {
             LoginScreen(
